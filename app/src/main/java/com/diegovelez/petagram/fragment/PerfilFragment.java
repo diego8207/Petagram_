2fd967efcbe657c278c5b89da814354991259900
Diego Armando Velez Peralta <si.macrosystem@gmail.com>
2017-04-10 19:00:22 -0500

    se implemento el CircularImageView

--- app/src/main/java/com/diegovelez/petagram/fragment/PerfilFragment.java
+++ app/src/main/java/com/diegovelez/petagram/fragment/PerfilFragment.java
@@ -1,10 +1,10 @@
 package com.diegovelez.petagram.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +13,7 @@ import android.view.ViewGroup;
 import com.diegovelez.petagram.R;
 import com.diegovelez.petagram.adapter.MascotaPerfilAdaptador;
 import com.diegovelez.petagram.pojo.Mascota;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.util.ArrayList;
 
@@ -24,6 +25,7 @@ public class PerfilFragment extends Fragment {
 
     private ArrayList<Mascota> mascotas;
     private RecyclerView rvListaMascotas;
+    private CircularImageView circularImageView;
 
 
     @Override
@@ -31,6 +33,13 @@ public class PerfilFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_perfil, container, false);
         rvListaMascotas = (RecyclerView) v.findViewById(R.id.rvMascotasPerfil);
+        circularImageView = (CircularImageView) v.findViewById(R.id.cimgPet);
+        circularImageView.setBorderWidth(10);
+        // Add Shadow with default param
+        circularImageView.addShadow();
+        // or with custom param
+        circularImageView.setShadowRadius(15);
+        circularImageView.setShadowColor(Color.BLUE);
 
        // LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         //llm.setOrientation(LinearLayoutManager.VERTICAL);
